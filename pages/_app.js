e287e2914a97d38082f7ed5ad1095e9aba3fc623
My Laptop <MOE_Admin@MOHDLAPTOP>
2023-07-25 04:14:05 +0300

    add "google auth"

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,44 @@
+/* eslint-disable @next/next/inline-script-id */
 import RootLayout from "@/components/Layout/RootLayout";
 import "@/styles/globals.css";
 import customTheme from "@/styles/theme";
 import { ChakraProvider } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import Script from "next/script";
 import "swiper/css";
-
-export default function App({ Component, pageProps, router }) {
+import { useEffect } from "react";
+import * as gtag from "../utils/gtag";
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <ChakraProvider theme={customTheme}>
         <RootLayout>
           <Component {...pageProps} />
