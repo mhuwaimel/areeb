0a597a45d49d761caee5daf016c76cc7129f3ab3
My Laptop <MOE_Admin@MOHDLAPTOP>
2023-07-25 03:37:32 +0300

    add "Google verification"

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="G5-YxlawZ0uWzgIWD2QTtG8Qr_6y_npPXSAUAlTJI_A"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  const allUsers = await prisma.projects.findMany();
+  res.status(200).json({ name: allUsers });
 }
