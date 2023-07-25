a283d6cd6a296111bd25e3da6036cffeb220a35c
My Laptop <MOE_Admin@MOHDLAPTOP>
2023-07-25 04:05:07 +0300

    fix "seo"

--- components/common/Seo.jsx
+++ components/common/Seo.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable @next/next/inline-script-id */
+/* eslint-disable @next/next/no-script-component-in-head */
 import Head from "next/head";
+import Script from "next/script";
 
 const Seo = ({ pageTitle }) => (
   <>
@@ -36,6 +39,21 @@ const Seo = ({ pageTitle }) => (
       />
 
       <link rel="icon" href="/favicon.ico" />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
     </Head>
   </>
 );
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/inline-script-id */
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
@@ -10,22 +9,6 @@ export default function Document() {
           name="google-site-verification"
           content="G5-YxlawZ0uWzgIWD2QTtG8Qr_6y_npPXSAUAlTJI_A"
         />
-
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-
-        <Script strategy="lazyOnload">
-          {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-        </Script>
       </Head>
 
       <body>
